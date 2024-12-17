1aed314085072f621f7825e37cf572938a75a315
santiago jimenez <santjimenez@utp.edu.co>
2024-12-17 14:18:31 -0500

    vercel analitics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
-import { inject } from '@vercel/analytics'
-
-inject() 
 
 const app = mount(App, {
   target: document.getElementById('app'),
