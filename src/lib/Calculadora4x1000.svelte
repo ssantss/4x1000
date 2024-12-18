921263a633e0a98a975458942d38413e451bac19
santiago jimenez <santjimenez@utp.edu.co>
2024-12-18 14:05:50 -0500

    okW

--- src/lib/Calculadora4x1000.svelte
+++ src/lib/Calculadora4x1000.svelte
@@ -244,7 +244,7 @@
             <div class="relative">
               <span class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400">$</span>
               <input
-                type="tel"
+                type="number"
                 inputmode="numeric"
                 pattern="[0-9]*"
                 id="monto"
